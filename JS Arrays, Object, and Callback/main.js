f32088242748408e674bc2a83d517175f2575e8e
zulfuqar1 <zulfugarga@code.edu.az>
2023-05-05 02:03:03 +0400

    task

---
+++ JS Arrays, Object, and Callback/main.js
@@ -0,0 +1,19 @@
+//(1)
+//const points  = [-4, -1, 2, 1, 7, 6];
+//points.sort(function(a, b) {return a - b});
+//console.log(points)
+
+
+//(2)
+//const arrays = [[1,2,3], [4,5,6,7]]
+//const newArr = arrays.flatMap((arr,i) => {return arr})
+//console.log(newArr)
+
+
+//(3)
+//var array = [0,NaN, 15, false, -22, '',undefined, 47, null]
+//const newArray = array.filter((number) => 
+//{
+//    return !isNaN(number) && number !== 0 && number !== false && number !== '' && number !== undefined && number !== null;
+//});
+//console.log(newArray)
